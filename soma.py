477eca0371120a85b29964bce511491a2b7853be
Victor <victorg.dias@estudante.cear.ufpb.br>
2026-03-23 16:19:04 -0300

    Add soma.py

---
+++ soma.py
@@ -0,0 +1,8 @@
+def soma(x, y):
+    return (x+y)
+
+
+a = int(input("Digite o primeiro valor: "))
+b = int(input("Digite o segundo valor: "))
+c = soma(a, b)
+print("A soma entre {} e {} é {}".format(a, b, c))
