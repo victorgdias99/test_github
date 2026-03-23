12f23f0aaf3b949229d94e64f790552e905ae241
Victor <victorg.dias@estudante.cear.ufpb.br>
2026-03-23 16:20:02 -0300

    Add sub.py Add function which subtract 2 int numbers

---
+++ sub.py
@@ -0,0 +1,8 @@
+def sub(x, y):
+    return (x-y)
+
+
+a = int(input("Digite o primeiro valor: "))
+b = int(input("Digite o segundo valor: "))
+c = sub(a, b)
+print("A subtração entre {} e {} é {}".format(a, b, c))
