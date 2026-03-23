74244022435f489eebd4d53aca766533eb50760a
Victor <victorg.dias@estudante.cear.ufpb.br>
2026-03-23 18:30:48 -0300

    Add div.py and modify soma.py

---
+++ div.py
@@ -0,0 +1,7 @@
+def div(x, y):
+    return x/y
+
+a = int(input("Digite o primeiro valor: "))
+b = int(input("Digite o segundo valor: "))
+c = div(a, b)
+print("A divisão entre {} e {} é {}".format(a, b, c))
--- soma.py
+++ soma.py
@@ -1,8 +1,9 @@
-def soma(x, y):
-    return (x+y)
+def soma(x, y, z):
+    return (x+y+z)
 
 
 a = int(input("Digite o primeiro valor: "))
 b = int(input("Digite o segundo valor: "))
-c = soma(a, b)
+c = int(input("Digite o terceiro valor: "))
+d = soma(a, b, c)
 print("A soma entre {} e {} é {}".format(a, b, c))
