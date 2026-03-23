e22adf20aaac6c06d26fc9d759e79cb8ddd303e8
Victor <victorg.dias@estudante.cear.ufpb.br>
2026-03-23 17:44:20 -0300

    Add mult.py
    Add function which multiply 2 int numbers

---
+++ mult.py
@@ -0,0 +1,7 @@
+def mult(x, y):
+    return x*y
+
+a = int(input("Digite o primeiro valor: "))
+b = int(input("Digite o segundo valor: "))
+c = mult(a, b)
+print("A multiplicação entre {} e {} é {}".format(a, b, c))
